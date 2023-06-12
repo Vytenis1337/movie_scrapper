a19f6006f8a5d91e0396da2656c58d265f06c08c
RicardasN <rihaddas@gmail.com>
2023-06-12 16:19:42 +0300

    fix: react-native-app (potentially)
    It works for Evaldas, please report if it does not for you.

--- packages/react-native-app/metro.config.js
+++ packages/react-native-app/metro.config.js
@@ -1,44 +1,25 @@
-const exclusionList = require('metro-config/src/defaults/exclusionList');
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
 const path = require('path');
 
-module.exports = (config) => {
-    const projectPath = process.cwd();
+const watchFolders = [
+  path.resolve(`${__dirname}/../../packages`),
+  path.resolve(__dirname, '../../node_modules'),
+  path.resolve(`${__dirname}/../`),
+];
 
-    const watchFolders = [path.resolve(projectPath, 'node_modules')];
-
-    const monoRootPath = path.resolve(__dirname, '../../');
-    watchFolders.push(path.resolve(monoRootPath, 'node_modules'));
-    watchFolders.push(path.resolve(monoRootPath, 'packages'));
-
-    if (config?.watchFolders?.length) {
-        watchFolders.push(...config.watchFolders);
-    }
-
-    const cnf = {
-        ...config,
-        transformer: {
-            getTransformOptions: async () => ({
-                transform: {
-                    // this defeats the RCTDeviceEventEmitter is not a registered callable module
-                    inlineRequires: true,
-                },
-            }),
-            ...(config?.transformer || {}),
-        },
-        resolver: {
-            blockList: exclusionList([
-                /projectConfig\/.*/,
-                /metro.config.local.*/,
-                /projectConfig\/.*/,
-                /website\/.*/,
-                /metro.config.local.*/,
-            ]),
-            ...(config?.resolver || {}),
-            extraNodeModules: config?.resolver?.extraNodeModules,
-        },
-        watchFolders,
-        projectRoot: path.resolve(projectPath),
-    };
-
-    return cnf;
+module.exports = {
+  watchFolders,
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
 };
